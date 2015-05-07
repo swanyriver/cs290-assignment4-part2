f95a7595804ee7c87da964a5899a954974500b0d
Brandon Swanson Ubuntu <swanyriver@gmail.com>
2015-05-06 22:35:30 -0700

    started html, tested dropdown menu, tested using hidden input to distingush form being submited, and tested reciept of post values

--- src/videostore.php
+++ src/videostore.php
@@ -10,6 +10,35 @@ if ($mysqli->connect_errno || $mysqli->connect_error)
   return;
 }
 
-echo "SUCCESS";
-
+$mysqli->query("CREATE TABLE IF NOT EXISTS records (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  category VARCHAR(255) NOT NULL,
+  length INT UNSIGNED NOT NULL,
+  rented BOOL DEFAULT FALSE
+  )"); 
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>[CS290] PHP part 2</title>
+        <style type="text/css">
+          th {font-weight: bold;}
+          th,td {padding:5px; border: solid 1px;}
+          table {border-collapse: collapse; border: solid thick;}
+        </style>
+    </head>
+    <body>
+        <form action = "http://web.engr.oregonstate.edu/~osterbit/2/repo/class-content/form_tests/Formtest.php" method ="post">
+          <input type="hidden" name="ACTION" value="categoryfilter" />
+          <select name="category">
+            <option value="testa">testa</option>
+            <option value="testb">testb</option>
+            <option value="testc">testc</option>
+          </select>
+          <input type="submit" value="Filter Videos by Category" />
+        </form>
+    </body>
+</html>
